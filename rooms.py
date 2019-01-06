b26201d3897075e934815be1569685bac5d3bd49
Meepster Hooves <42324391+Meepsterkh@users.noreply.github.com>
2019-01-06 12:18:53 -0800

    Create rooms.py

---
+++ rooms.py
@@ -0,0 +1,38 @@
+
+#      "--------
+plank = "pppppppp" \
+       "pppppppp" \
+       "pppppppp" \
+       "pppppppp" \
+       "pppppppp" \
+       "pppppppp"
+
+grass = "gggggggg" \
+        "gggggggg" \
+        "gggggggg" \
+        "gggggggg" \
+        "gggggggg" \
+        "gggggggg"
+
+tile = "tttttttt" \
+       "tttttttt" \
+       "tttttttt" \
+       "tttttttt" \
+       "tttttttt" \
+       "tttttttt"
+
+tileF = "TTTTTTTT" \
+        "TTTTTTTT" \
+        "TTTTTTTT" \
+        "TTTTTTTT" \
+        "TTTTTTTT" \
+        "TTTTTTTT"
+
+
+homeBottom = "tttppppp" \
+        "tttppppp" \
+        "tttppppp" \
+        "ttpppppp" \
+        "pppppppp" \
+        "pppppppp"
+
